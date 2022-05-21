12fa92b299d2de732d5ffadb0098f74e888c4e0a
aristide notouom <fotso0244@gmail.com>
2022-05-21 12:46:02 -0700

    How to print with format b, u, x, X, o

--- _printf.c
+++ _printf.c
@@ -51,7 +51,7 @@ int print_format_s(char *str, int nb)
  */
 int check(char c)
 {
-	char *s = "%cdebfgiosux";
+	char *s = "%cdebXfgiosux";
 
 	while (*s != '\0')
 	{
@@ -62,12 +62,33 @@ int check(char c)
 	}
 	return (0);
 }
-int print_format_b(long int i, int nb)
+/**
+ * print_format_box - prints in format b, o, x and X
+ * @i: an integer
+ * @f: a format identifier
+ * @nb: a counter
+ *
+ * Return: a counter
+ */
+int print_format_box(long int i, char f, int nb)
 {
 	char *s, *p;
 	int size = -1, new_nb = nb, j;
-	long int mod = 0, div = i;
+	long int c, mod = 0, base, div = i;
 
+	if (i < 0)
+	{
+		div = UINT_MAX + 1 + i;
+	}
+	else
+		div = i;
+	c = div;
+	if (f == 'b')
+		base = 2;
+	if (f == 'o')
+		base = 8;
+	if (f == 'x' || f == 'X')
+		base = 16;
 	if (i == 0)
 	{
 		write(1, "0", 1);
@@ -75,29 +96,35 @@ int print_format_b(long int i, int nb)
 	}
 	while (div != 0)
 	{
-		div /= 2;
+		div /= base;
 		size++;
 	}
 	size++;
-	if (i < 0)
-	{
-		div = UINT_MAX + 1 + i;
-		/*size++;*/
-	}
-	else
-		div = i;
+	div = c;
 	s = malloc(sizeof(*s) * size);
 	if (s != NULL)
 	{
 		for (j = 0; j <= size - 2; j++)
 		{
-			mod = div % 2;
-			div /= 2;
-			s[j] = mod + 48;
+			mod = div % base;
+			div /= base;
+			if (mod < 10)
+				s[j] = mod + 48;
+			else
+				if (f == 'x')
+					s[j] = mod + 87;
+				else
+					if (f == 'X')
+						s[j] = mod + 55;
 		}
-		s[j] = div + 48;
-		/*if (i < 0)
-			s[size - 1] = '-';*/
+		if (div < 10)
+			s[j] = div + 48;
+		else
+			if (f == 'x')
+				s[j] = mod + 87;
+			else
+				if (f == 'X')
+					s[j] = mod + 55;
 		s[size] = '\0';
 		j = 0;
 		p = s + size - 1;
@@ -114,18 +141,31 @@ int print_format_b(long int i, int nb)
 	return (new_nb);
 }
 /**
- * print_format_di - prints an integer
+ * print_format_diu - prints an integer
  * @i: an integer to print
+ * @f: a format identifier
  * @nb: a counter
  *
  * Return: a counter
  */
-int print_format_di(int i, int nb)
+int print_format_diu(long int i, char f, int nb)
 {
 	char *s, *p;
 	int new_nb = nb, j, size = -1;
-	long int mod = 0, div = (long int)i;
+	long int mod = 0, c, div = i;
 
+	if (i < 0 && f == 'u')
+		div = UINT_MAX + 1 + i;
+	
+	if (i < 0 && (f == 'd' || f == 'i'))
+	{
+		div = i * (-1);
+		size++;
+	}
+	else
+		if (f == 'd' || f == 'i')
+			div = i;
+	c = div;
 	if (i == 0)
 	{
 		write(1, "0", 1);
@@ -137,13 +177,7 @@ int print_format_di(int i, int nb)
 		size++;
 	}
 	size++;
-	if (i < 0)
-	{
-		div = (long int)i * (-1);
-		size++;
-	}
-	else
-		div = (long int)i;
+	div = c;
 	s = malloc(sizeof(*s) * (size + 1));
 	if (s != NULL)
 	{
@@ -206,10 +240,10 @@ int _printf(const char *format, ...)
 					nb = print_format_c(va_arg(ap, int), nb), i++;
 				if (format[i] == 's')
 					nb = print_format_s(va_arg(ap, char *), nb), i++;
-				if (format[i] == 'd' || format[i] == 'i')
-					nb = print_format_di(va_arg(ap, int), nb), i++;
-				if (format[i] == 'b')
-					nb = print_format_b(va_arg(ap, long int), nb), i++;
+				if (format[i] == 'd' || format[i] == 'i' || format[i] == 'u')
+					nb = print_format_diu(va_arg(ap, long int), format[i], nb), i++;
+				if (format[i] == 'b' || format[i] == 'X' || format[i] == 'o' || format[i] == 'x')
+					nb = print_format_box(va_arg(ap, long int), format[i], nb), i++;
 			}
 		}
 	}
